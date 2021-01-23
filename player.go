otofune/hlsq

commit fde34d573d37337a2cd9f1125e5a7fdabd74a606

retrying is no need to get master playlist
user can retry soon

player.go
@@ -40,12 +40,16 @@ func (s *playSession) Wait() error {
 }
 
 func Play(ctx context.Context, hc *http.Client, playlistURL *url.URL, fmpv FilterMediaPlaylistVariantFn, ph PlayHandler) (PlaySession, error) {
-	resp, err := repeahttp.Get(ctx, hc, playlistURL)
+	resp, err := ctxGet(ctx, hc, playlistURL)
 	if err != nil {
 		return nil, err
 	}
 	resp.Body = ctxdebugfs.Tee(ctx, resp.Body, "master.m3u8")
 	defer resp.Body.Close()
+
+	if resp.StatusCode > 399 {
+		return nil, fmt.Errorf("Failed to get master playlist: server returns %s", resp.Status)
+	}
 
 	playlist, err := decodeM3U8(resp.Body)
 	if err != nil {

repeahttp/do.go
@@ -15,22 +15,27 @@ func ctxGet(ctx context.Context, hc *http.Client, u *url.URL) (*http.Response, e
 	return hc.Do(req)
 }
 
+const retryTimes = 5
+
 func Get(ctx context.Context, hc *http.Client, u *url.URL) (resp *http.Response, err error) {
-	for i := time.Duration(0); i < 5; i++ {
+	for i := time.Duration(0); i < retryTimes; i++ {
 		select {
 		case <-ctx.Done():
 			return nil, ctx.Err()
 		default:
 		}
 
-		sec := (1 << i) >> 1 * time.Second
-		time.Sleep(sec)
-
 		if resp != nil {
 			resp.Body.Close()
 		}
 
-		resp, err = ctxGet(ctx, hc, u)
+		sec := ((1 << i) >> 1) * time.Second
+		time.Sleep(sec)
+
+		cctx, cancel := context.WithTimeout(ctx, time.Second*30)
+		defer cancel()
+
+		resp, err = ctxGet(cctx, hc, u)
 		if err != nil {
 			continue
 		}

util.go
@@ -1,0 +1,15 @@
+package hlsq
+
+import (
+	"context"
+	"net/http"
+	"net/url"
+)
+
+func ctxGet(ctx context.Context, hc *http.Client, u *url.URL) (*http.Response, error) {
+	req, err := http.NewRequestWithContext(ctx, "GET", u.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+	return hc.Do(req)
+}

commit 0070c871e578400eabd339790470ed5b4d1976a7

:bug: repeahttp: Remove timeout control
This causes unexpected cancel when reading resp.Body.
This bug was introduced from fde34d573d37337a2cd9f1125e5a7fdabd74a606

player.go
@@ -101,7 +101,6 @@ INFINITE_LOOP:
 		select {
 		case <-ctx.Done():
 			if err := ctx.Err(); err != nil {
-
 				return xerrors.Errorf("%w", err)
 			}
 			return nil

repeahttp/do.go
@@ -34,9 +34,7 @@ func Get(ctx context.Context, hc *http.Client, u *url.URL) (resp *http.Response,
 		sec := ((1 << i) >> 1) * time.Second
 		time.Sleep(sec)
 
-		cctx, cancel := context.WithTimeout(ctx, time.Second*30)
-		resp, err = ctxGet(cctx, hc, u)
-		cancel()
+		resp, err = ctxGet(ctx, hc, u)
 		if err != nil {
 			err = xerrors.Errorf("%w", err)
 			continue